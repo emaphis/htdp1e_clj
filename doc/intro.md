eb12ead11e8445b77883ea57c0cce3b3ac69e655
Ed Maphis <emaphis85@gmail.com>
2019-12-13 23:17:25 -0500

    Initial check-in.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to htdp1e-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject htdp1e-clj "0.1.0-SNAPSHOT"
+  :description "How To Design Programs 1e - Clojure"
+  :url "http://github.com/emaphis/htdp1e-clj"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns htdp1e-clj.core})
